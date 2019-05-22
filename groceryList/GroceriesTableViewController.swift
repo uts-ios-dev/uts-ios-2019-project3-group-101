c25bcc10e162d2381b04e58e6a85562caa4f238d
Divraj <divrajsingh@hotmail.com>
2019-05-22 15:42:00 +1000

    Added project
    Tableview and core data implemented

---
+++ groceryList/GroceriesTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  GroceriesTableViewController.swift
+//  groceryList
+//
+//  Created by Divraj Singh on 22/5/19.
+//  Copyright © 2019 Divraj Singh. All rights reserved.
+//
+
+import UIKit
+
+extension ItemEntity {
+    
+    func toItem() -> Item {
+        return Item(item: self.item!)
+    }
+    
+}
+
+class GroceriesTableViewController: UITableViewController {
+
+    @IBOutlet var groceryTV: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    @IBAction func handleAdd(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        groceryTV.insertRows(at: [IndexPath], with: false)
+    }
+    
+    // MARK: - Table view data source
+    var myItemsFromCoreData = [Item]()
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return myItemsFromCoreData.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groceryItemCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = myItemsFromCoreData[indexPath.row].item
+
+        return cell
+    }
+ 
+
+}
